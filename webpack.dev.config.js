6925b7b8c270e9cf3348d7ddfd6993fb819b5a9e
angelhernandez1001 <143427220+angelhernandez1001@users.noreply.github.com>
2024-03-26 04:08:15 +0000

    ⭐Configuracion Webpack

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,11 +1,16 @@
 const path = require('path');
 
 module.exports ={
-    mode: "production",
+    mode: "evelopment",
     entry: "./client/index.js",
     output:{
         path: path.resolve(__dirname, "public"),
         filename: "bundle.js",
         publicPath: "/"
+    },
+    devServer:{
+        static: path.join(__dirname, "public"),
+        port: 8080,
+        host: "0.0.0.0"
     }
 };
